ccec19cb446f6fde3c7de2b0f774f1af27cb5ad5
imperialempressM0r1 <112771744+imperialempressM0r1@users.noreply.github.com>
2022-12-01 10:23:12 +0100

    Add files via upload

---
+++ aoc_day0.R
@@ -0,0 +1,13 @@
+library(readr); library(stringi)
+data <- read_file("input_day0.txt")
+hm <- stri_split_lines1(data)
+c = 0; arr0 = c()
+for(i in 1:length(hm)){
+  if(hm[i]!=""){
+    c = c + as.numeric(hm[i])
+  } else {
+    arr0 = append(arr0, c); c = 0
+  }
+}
+arr0 <- sort(arr0)
+arr0[length(arr0)]; arr0[length(arr0)]+ arr0[length(arr0)-1]+ arr0[length(arr0)-2]
