4587fa95bd9243c9278e9d4731f004e2a5f10c01
imperialempressM0r1 <112771744+imperialempressM0r1@users.noreply.github.com>
2022-12-03 06:49:47 +0100

    Update aoc_day2.py

--- day2/aoc_day2.py
+++ day2/aoc_day2.py
@@ -1,23 +1,14 @@
 with open("day2_input.txt", "r") as file:
     s = file.read().split("\n")
+def score(x):
+    if x.isupper():
+        d += ord(a)-38
+    else:
+        d += ord(a)-96
 first, second = "", ""
-elves = ""
-c,d = 0,0
+p1, p2 = 0,0
 already = []
 line = 2
-for i in s:
-    if (line+1)%3 == 0:
-        for a in s[line-2]:
-            if a in s[line-1] and a in s[line] and a not in already:
-                already += [a]
-                if a.isupper():
-                    d += ord(a)-38
-                else:
-                    d += ord(a)-96
-    line += 1
-    already = []
-
-
 for i in s:
     first = i[:int((len(i)/2))]
     second = i[int((len(i)/2)):]
@@ -25,11 +16,14 @@ for i in s:
     for j in first:
         if j in second and j not in already:
             already += [j]
-            if j.isupper():
-                c += ord(j)-38
-                # print(j, ord(j)-38)
-            else:
-                c += ord(j)-96
-                # print(j, ord(j)-96)   
+            p1 += score(j)
+for i in s:
+    if (line+1)%3 == 0:
+        for a in s[line-2]:
+            if a in s[line-1] and a in s[line] and a not in already:
+                already += [a]
+                p2 += score(a)
+    line += 1
+    already = []
 
-print(c, d)
+print(p1, p2)
