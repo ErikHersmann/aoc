be573534ab868b21e7a8754008e574bd017f9999
m0r1 <112771744+mori-peepoSit@users.noreply.github.com>
2022-12-06 06:23:10 +0100

    Update aoc_day5.py

--- day5/aoc_day5.py
+++ day5/aoc_day5.py
@@ -11,22 +11,3 @@ for i in s:
     if len(set((s[c-13], s[c-12], s[c-11], s[c-10], s[c-9], s[c-8], s[c-7],s[c-6],s[c-5],s[c-4] ,s[c-3], s[c-2], s[c-1], s[c]))) == 14:
         print(c+1)
         break
-
-
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-# a = [3, 4 , 5]; print(a[-2:]) # last n elements; print(a[::-1]) # reverse array
