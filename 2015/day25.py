803e29c2314dd29cb712ea2e7f638a7f9f11ac70
suvoms <no@mail.com>
2026-06-13 09:43:57 +0200

    idek

--- 2015/day25.py
+++ 2015/day25.py
@@ -1,14 +1,13 @@
-
-
-
 def get_next(coordinate):
     (row, col) = coordinate
     if row == 1:
-        return (col+1, 1)
-    return (row-1, col+1)
+        return (col + 1, 1)
+    return (row - 1, col + 1)
+
 
 def calculate(value):
-    return (value * 252533 ) % 33554393
+    return (value * 252533) % 33554393
+
 
 target = (2981, 3075)
 coord = (1, 1)
@@ -16,4 +15,4 @@ prev = 20151125
 while coord != target:
     coord = get_next(coord)
     prev = calculate(prev)
-print(prev)
+print(f"Part 1: {prev}")
