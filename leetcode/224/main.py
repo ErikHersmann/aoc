2778f4ae902865b2c5d215bc8c50597e1af0bbd9
suvoms <no@mail.com>
2026-07-24 18:46:37 +0200

    Cleaned up 224

--- leetcode/224/main.py
+++ leetcode/224/main.py
@@ -2,92 +2,53 @@ from collections import deque
 
 
 class Solution:
-    def calculate(self, s: str) -> int:
-        stack = deque()
-        n = len(s)
-        ptr = 0
-        if s[0] == "-":
-            stack.append("0")
-            stack.append("-")
-            ptr += 1
-        while ptr < n:
-            character = s[ptr]
-            temp = ""
-            while character.isnumeric():
-                temp += character
-                ptr += 1
-                if ptr >= n:
-                    break
-                character = s[ptr]
-                continue
-            if len(temp) > 0:
-                while True:
-                    if len(stack) <= 1:
-                        stack.append(temp)
-                        break
-                    top = stack.pop()
-                    if top == "+":
-                        stack.append(str(int(stack.pop()) + int(temp)))
-                        temp = stack.pop()
-                    elif top == "-":
-                        left = stack.pop()
-                        if left == "(":
-                            stack.append("(")
-                            stack.append(str(0 - int(temp)))
-                        else:
-                            assert left.isnumeric() or (left[0] == "-" and left[1:].isnumeric()), left
-                            stack.append(str(int(left) - int(temp)))
-                        temp = stack.pop()
-                    else:
-                        stack.append(top)
-                        stack.append(temp)
-                        break
-            match character:
-                case "(":
-                    stack.append(character)
-                case ")":
-                    temp = ""
-                    stack.append(character)
-                    top = stack.pop()
-                    while top != "(":
-                        top = stack.pop()
-                        if top != "(":
-                            temp += top
-                    # stack.append(temp)
-                    # TODO: temp must be numeric so treat it like the numeric above
-                    if len(temp) > 0:
-                        # We have a number so maybe we can simplify
-                        # Loop and check if u have more to simplify
-                        while True:
-                            if len(stack) <= 1:
-                                stack.append(temp)
-                                break
-                            top = stack.pop()
-                            if top == "+":
-                                stack.append(str(int(stack.pop()) + int(temp)))
-                                temp = stack.pop()
-                            elif top == "-":
-                                left = stack.pop()
-                                if left == "(":
-                                    stack.append("(")
-                                    stack.append(str(0 - int(temp)))
-                                else:
-                                    assert left.isnumeric() or (left[0] == "-" and left[1:].isnumeric()), left
-                                    stack.append(str(int(left) - int(temp)))
-                                temp = stack.pop()
-                            else:
-                                stack.append(top)
-                                stack.append(temp)
-                                break
+    def resolve_stack(self, temp):
+        if type(temp) == str and len(temp) == 0:
+            return
+        while True:
+            if len(self.stack) <= 1:
+                self.stack.append(temp)
+                break
+            top = self.stack.pop()
+            match top:
                 case "+":
-                    stack.append(character)
+                    self.stack.append(int(self.stack.pop()) + int(temp))
+                    temp = self.stack.pop()
                 case "-":
-                    stack.append(character)
-                case " ":
-                    pass
+                    self.stack.append((0 if self.stack[-1] == "(" else int(self.stack.pop())) - int(temp))
+                    temp = self.stack.pop()
                 case default:
-                    # We are at the end ?
-                    continue
-            ptr += 1
-        assert len(stack) == 1
-        return int(stack[0])
+                    self.stack.append(top)
+                    self.stack.append(temp)
+                    break
+
+    def calculate(self, s: str) -> int:
+        self.stack = deque()
+        pointer = 0
+        if s[0] == "-":
+            self.stack.append(0)
+            self.stack.append("-")
+            pointer += 1
+        while pointer < len(s):
+            character = s[pointer]
+            if character == " ":
+                pointer += 1
+                continue
+            current_number = ""
+            while character.isnumeric():
+                current_number += character
+                pointer += 1
+                if pointer >= len(s):
+                    break
+                character = s[pointer]
+            self.resolve_stack(current_number)
+            match character:
+                case ")":
+                    current_number = self.stack.pop()
+                    assert self.stack.pop() == "("
+                    self.resolve_stack(current_number)
+                case "+" | "-" | "(":
+                    self.stack.append(character)
+            pointer += 1
+        return int(self.stack[0])
+
--- leetcode/224/test.py
+++ leetcode/224/test.py
@@ -1,4 +1,4 @@
 from main import Solution
 
 sol = Solution()
-sol.calculate("(6)-(8)-(7)+(1+(6))")
+sol.calculate("(1+(4+5+2)-3)+(6+8)")
